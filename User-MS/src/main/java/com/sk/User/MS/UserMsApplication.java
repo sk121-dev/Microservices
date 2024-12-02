ed6e7a01d498807bf51600f9438da9cdc62cd61e
SarojChaudhary121 <99866673+SarojChaudhary121@users.noreply.github.com>
2024-12-03 05:02:59 +0530

    Adding user service

---
+++ User-MS/src/main/java/com/sk/User/MS/UserMsApplication.java
@@ -0,0 +1,13 @@
+package com.sk.User.MS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMsApplication.class, args);
+	}
+
+}
